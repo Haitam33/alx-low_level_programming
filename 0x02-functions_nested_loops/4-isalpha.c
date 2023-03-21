1780546eac064581c500be0621e4e29f99c6d614
Haitam33 <haitam.mastaki@gmail.com>
2023-03-21 16:13:55 -0700

    4. isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * _islower - checks for lowercase
+  * @c: the number to be checked
+  *
+  * Return: 1 if the character is lower or 0 if not
+  */
+int _isalpha(int c)
+{
+	if (c == 'c' || c == 'C')
+		return (1);
+	return (0);
+}
