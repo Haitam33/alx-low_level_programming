6aaaf952034e6fcbef316298add875cbb8ddc926
Haitam33 <haitam.mastaki@gmail.com>
2023-03-21 13:44:42 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+  * main -  prints "_putchar"
+  *
+  * Return: Always 0 (secusess)
+  */
+int main(void)
+{
+	int x = 0;
+	char c[8] = "_putchar";
+	int x = 0;
+
+	while (x < 8)
+	{
+		putchar (c[i]);
+		x++;
+	}
+	putchar ('\n');
+	return (0);
+}
