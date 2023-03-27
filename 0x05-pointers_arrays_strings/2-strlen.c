05b65cd634af033c2905593ad1ac1541d184e816
Haitam33 <haitam.mastaki@gmail.com>
2023-03-27 04:19:23 -0700

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *  strlen - returns the length of a string.
+ *
+ * @s: input.
+ *
+ **/
+
+int _strlen(char *s)
+{
+	int i = 0;
+	while (i != '\0')
+	{
+		i++;
+	}
+	return(i);
+}
