4cd3e4cc29cd6e52209b731c5e456bf9f3019c90
Haitam33 <haitam.mastaki@gmail.com>
2023-04-04 18:30:30 -0700

    5. strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -3,7 +3,7 @@
  * _strstr -  locates a substring.
  * @haystack: input string.
  * @needle: input substring.
- * Return: pointer to the beginning of the located substring, or NULL if not found.
+ * Return: pointer to the beginning of substring, or NULL if not found.
  */
 char *_strstr(char *haystack, char *needle)
 {
