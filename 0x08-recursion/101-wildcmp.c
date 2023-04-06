d233d46afbe3361aca56287dcce35fa20a9deced
Haitam33 <haitam.mastaki@gmail.com>
2023-04-05 19:57:48 -0700

    8. Inception. Now, before you bother telling me it's impossible...

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * checkstr - check if two strings are identical.
+ * @s1: first string.
+ * @s2: 2nd string.
+ * @i: left index.
+ * @j: special index. (joker)
+ * Return: 1 if s is palindrome, 0 otherwise.
+ */
+int checkstr(char *s1, char *s2, int i, int j)
+{
+	if (s1[i] == '\0' && s2[j] == '\0')
+		return (1);
+	if (s1[i] == s2[j])
+		return (checkstr(s1, s2, i + 1, j + 1));
+	if (s1[i] == '\0' && s2[j] == '*')
+		return (checkstr(s1, s2, i, j + 1));
+	if (s2[j] == '*')
+		return (checkstr(s1, s2, i + 1, j) || str_checker(s1, s2, i, j + 1));
+	return (0);
+}
+/**
+ * wildcmp - returns 1 if identical, 0 if not.
+ * @s1: first string.
+ * @s2: 2nd string.
+ *
+ * Return: 1 if are considered identical.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	return (checkstr(s1, s2, 0, 0));
+}
