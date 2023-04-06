aa5d0f50b2836d956a7265b213fc594f9394d92e
Haitam33 <haitam.mastaki@gmail.com>
2023-04-05 19:59:05 -0700

    8. Inception. Now, before you bother telling me it's impossible...

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -17,7 +17,7 @@ int checkstr(char *s1, char *s2, int i, int j)
 	if (s1[i] == '\0' && s2[j] == '*')
 		return (checkstr(s1, s2, i, j + 1));
 	if (s2[j] == '*')
-		return (checkstr(s1, s2, i + 1, j) || str_checker(s1, s2, i, j + 1));
+		return (checkstr(s1, s2, i + 1, j) || checkstr(s1, s2, i, j + 1));
 	return (0);
 }
 /**
