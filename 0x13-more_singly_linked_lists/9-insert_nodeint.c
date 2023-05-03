e251b97cd8dd1c4ce2a7de198a7fa47548fb5409
Haitam33 <haitam.mastaki@gmail.com>
2023-05-03 16:12:31 -0700

    9. Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int n;
+	unsigned int x;
 	listint_t *current, *new;
 
 	if (head == NULL)
@@ -19,7 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (idx != 0)
 	{
 		current = *head;
-		for (n = 0; n < idx - 1 && current != NULL; n++)
+		for (x = 0; x < idx - 1 && current != NULL; x++)
 		{
 			current = current->next;
 		}
